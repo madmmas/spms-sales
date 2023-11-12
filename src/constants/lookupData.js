51c80e4b2ee11dd6e3858444113fb3cc03b118cf
Moinuddin Masud <moinuddinmasud@gmail.com>
2023-11-12 20:15:06 +0000

    add default field in lookup data

--- src/constants/lookupData.js
+++ src/constants/lookupData.js
@@ -1,11 +1,11 @@
 export const BANK_CASH = [
-    { id: 'BANK', name: 'BANK' },
+    { id: 'BANK', name: 'BANK', default: true},
     { id: 'CASH', name: 'CASH' },
     { id: 'MFS', name: 'MFS' },
 ];
 
 export const BLOOD_GROUP = [
-    { id: 'A+', name: 'A+' },
+    { id: 'A+', name: 'A+', default: true},
     { id: 'A-', name: 'A-' },
     { id: 'B+', name: 'B+' },
     { id: 'B-', name: 'B-' },
@@ -16,7 +16,7 @@ export const BLOOD_GROUP = [
 ];
 
 export const CITIES = [
-    { id: 'Dhaka', name: 'Dhaka' },
+    { id: 'Dhaka', name: 'Dhaka', default: true},
     { id: 'Rajshahi', name: 'Rajshahi' },
     { id: 'Chitagong', name: 'Chitagong' },
     { id: 'Sylhet', name: 'Sylhet' },
@@ -24,7 +24,7 @@ export const CITIES = [
 ];
 
 export const CURRENCY = [
-    { id: 'BDT', name: 'BDT' },
+    { id: 'BDT', name: 'BDT', default: true},
     { id: 'INR', name: 'INR'},
     { id: 'USD', name: 'USD' },
     { id: 'EUR', name: 'EUR' },
@@ -34,48 +34,48 @@ export const CURRENCY = [
 ];
 
 export const COLLECTION_TYPES = [
-    { id: 'CASH', name: 'Cash' },
+    { id: 'CASH', name: 'Cash', default: true},
     { id: 'MFS', name: 'MFS' },
     { id: 'BANK', name: 'Bank Deposit' }    
 ];
 
 export const PAYMENT_TYPES = [
-    { id: 'CASH', name: 'Cash' },
+    { id: 'CASH', name: 'Cash', default: true},
     { id: 'MFS', name: 'MFS' },
     { id: 'BANK', name: 'Bank Deposit' }    
 ];
 
 
 export const CUSTOMER_CATEGORY = [
-    { id: 'WALKIN', name: 'Walk-in' },
-    { id: 'REGISTERED', name: 'Registered' },
-    { id: 'CONDITIONAL', name: 'Conditional' }
+    { id: 'WALKIN', name: 'Walk-in', default: true},
+    { id: 'REGISTERED', name: 'Registered'},
+    { id: 'CONDITIONAL', name: 'Conditional'}
 ];
 
 export const ADJUSTMENT_TYPE = [
-    { id: 'ADD', name: 'Addition' },
+    { id: 'ADD', name: 'Addition', default: true},
     { id: 'REDUCE', name: 'Reduction' }
 ];
 
 export const DISTRICT = [
-    { id: 'DHAKA', name: 'DHAKA' },
+    { id: 'DHAKA', name: 'DHAKA', default: true},
     { id: 'CUMILLA', name: 'CUMILLA' },
     { id: 'KHULNA', name: 'KHULNA' }
 ];
 
 export const GENDER = [
-    { id: 'MALE', name: 'Male' },
+    { id: 'MALE', name: 'Male', default: true},
     { id: 'FEMALE', name: 'Female' }
 ];
 
 export const EMPLOYMENT_STATUS = [
     { id: 'PART-TIMER', name: 'Part-Timer' },
     { id: 'CONTRACTUAL', name: 'Contractual' },
-    { id: 'PERMANENT', name: 'Permanent' }
+    { id: 'PERMANENT', name: 'Permanent', default: true}
 ];
 
 export const MARITAL_STATUS = [
-    { id: 'MARRIED', name: 'Married' },
+    { id: 'MARRIED', name: 'Married', default: true},
     { id: 'DIVORCED', name: 'Divorced' },
     { id: 'SEPARATED', name: 'Separated' },
     { id: 'WIDOWED', name: 'Widowed' },
@@ -83,20 +83,20 @@ export const MARITAL_STATUS = [
 ];
 
 export const MEASUREMENT_UNITS = [
-    { id: 'PCS', name: 'PCS' },
+    { id: 'PCS', name: 'PCS', default: true},
     { id: 'DZN', name: 'DZN' },
     { id: 'PKT', name: 'PKT' },
     { id: 'SET', name: 'SET' }
 ];
 
 export const PRODUCT_STATUS = [
-    { id: 'Active', name: 'Active' },
+    { id: 'Active', name: 'Active', default: true},
     { id: 'Inactive', name: 'Inactive' },
     { id: 'Draft', name: 'Draft' }
 ];
 
 export const RELIGION = [
-    { id: 'MUSLIM', name: 'Muslim' },
+    { id: 'MUSLIM', name: 'Muslim', default: true},
     { id: 'HINDU', name: 'Hindu' },
     { id: 'CHRISTIAN', name: 'Christian' },
     { id: 'BUDDHA', name: 'Buddha' },
