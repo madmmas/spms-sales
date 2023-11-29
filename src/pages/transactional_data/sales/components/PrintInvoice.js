1428813087c7a1e47f6001f5d236bbaa9a023009
RRaihan123 <ronyraihan141@gmail.com>
2023-11-29 16:33:43 +0600

    Fixed the payment in invoice for registered sales

--- src/pages/transactional_data/sales/components/PrintInvoice.js
+++ src/pages/transactional_data/sales/components/PrintInvoice.js
@@ -211,7 +211,8 @@ export const PrintInvoice = () => {
                             (-) Payment
 
                         </td>
-                        <td className="price right-align">{ Number.parseFloat(invoice.paid).toFixed(2)}</td>
+                        {invoice.customer_category !== "REGISTERED" && <td className="price right-align">{ Number.parseFloat(invoice.paid).toFixed(2)}</td>}
+                        {invoice.customer_category === "REGISTERED" && <td className="price right-align">{ Number.parseFloat(invoice.payment.cash_amount + invoice.payment.bank_amount + invoice.payment.mfs_amount).toFixed(2)}</td>}
                     </tr>}
                     
                     {invoice.customer_category === "CONDITIONAL" && invoice.balance_forward!==-99999999 && <tr>
@@ -233,11 +234,11 @@ export const PrintInvoice = () => {
 
                     {invoice.customer_category === "REGISTERED" && invoice.balance_forward ===-99999999 && <tr>
                         <th colSpan="7" className="total text line">(+) Balance</th>
-                        <th className="total price right-align line">{ Number.parseFloat(Number.parseFloat(invoice.net).toFixed(2)) - Number.parseFloat(Number.parseFloat(invoice.paid).toFixed(2)) }</th>
+                        <th className="total price right-align line">{ Number.parseFloat(Number.parseFloat(invoice.net).toFixed(2)) - Number.parseFloat(Number.parseFloat(invoice.payment.cash_amount + invoice.payment.bank_amount + invoice.payment.mfs_amount).toFixed(2)) }</th>
                     </tr>}
                     {invoice.customer_category === "REGISTERED" && invoice.balance_forward !==-99999999 && <tr>
                         <th colSpan="7" className="total text line">(+) Balance</th>
-                        <th className="total price right-align line">{ Number.parseFloat(Number.parseFloat(invoice.net).toFixed(2)) + Number.parseFloat(Number.parseFloat(invoice.balance_forward).toFixed(2)) -  Number.parseFloat(Number.parseFloat(invoice.paid).toFixed(2))}</th>
+                        <th className="total price right-align line">{ Number.parseFloat(Number.parseFloat(invoice.net).toFixed(2)) + Number.parseFloat(Number.parseFloat(invoice.balance_forward).toFixed(2)) -  Number.parseFloat(Number.parseFloat(invoice.payment.cash_amount + invoice.payment.bank_amount + invoice.payment.mfs_amount).toFixed(2))}</th>
                     </tr>}
                    
                 </tbody>
