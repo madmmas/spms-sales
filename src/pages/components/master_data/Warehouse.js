1673419871194a35f13656920edf46cbbe0c0035
RRaihan123 <ronyraihan141@gmail.com>
2023-12-30 11:51:32 +0600

    completed the global filtering for warehouse

--- src/pages/components/master_data/Warehouse.js
+++ src/pages/components/master_data/Warehouse.js
@@ -12,13 +12,28 @@ export default function Warehouse({
     return (
         <>
         <SelectMasterData field={field} modelName={WAREHOUSE_MODEL}
-            displayField="name"
+            caption='Select Warehouse'
+            displayField="name" showFields={["name"]}
             className={classNames({ 'p-invalid': fieldState.error })} 
             onSelect={(e) => {
                 onSelect(e);
             }}
+            defaultFilters={{
+                globalFilterFields: ['name', 'address', 'description'],
+                fields: fields,
+                first: 0,
+                rows: 10,
+                page: 1,
+                sortField: null,
+                sortOrder: null,
+                filters: {
+                    global: { value: null, matchMode: 'contains' },
+                }
+            }}
             columns={[
-                {field: 'name', header: 'Warehouse', filterPlaceholder: 'Filter by Warehouse'}, 
+                {field: 'name', header: 'Warehouse', filterPlaceholder: 'Filter by Warehouse'},
+                {field: 'address', header: 'Warehouse Address', filterPlaceholder: 'Filter by Warehouse Address'},
+                {field: 'description', header: 'Warehouse Description', filterPlaceholder: 'Filter by Warehouse Description'},
             ]} />
         </>
     )
